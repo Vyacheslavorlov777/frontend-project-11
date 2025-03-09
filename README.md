e8403be76df23bada79c4bed3bea3ce91d4e587f
Vyacheslavorlov777 <Vyacheslavorlov777@gmail.com>
2025-03-09 20:53:22 +0300

    start project, 1 stage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Vyacheslavorlov777/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Vyacheslavorlov777/frontend-project-11/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/2ef7427665f5b9ead762/maintainability)](https://codeclimate.com/github/Vyacheslavorlov777/frontend-project-11/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  output: {
+    clean: true,
+  },
+  ignoreWarnings: [
+    {
+      module: /module2\.js\?[34]/, // A RegExp
+    },
+    {
+      module: /[13]/,
+      message: /homepage/,
+    },
+    /warning from compiler/,
+    (warning) => true,
+  ],
+};
